7f6df1de238e250e3ae9b086ee74a6ca5b04eb0e
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-23 20:38:56 +0300

    post debug initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# PITCHES
+
+## Author
+* Anna Adhiambo
+
+## Description
+* An application that allows users to use that one minute wisely. The users will submit their one minute pitches and other users will vote on them and leave comments to give their feedback on them.
+Here is the [Link]() to my website
+
+## Features
+* Landing page containing wecoming message
+* Clickable categories for pitches
+* Posting and commenting on other pitches
+* User login/registration 
+
+## Technology Used
+* Python
+* Postgress
+* Flask Framework
+* Bootsrap
+* Pip
+* CSS
+
+## Support and contact details
+* contact@ annaadhiambo01@gmail.com
+
+## How to access it 
+You can access it by cloning the [Repository](https://github.com/annaadhiambo/The-Last-Pitch.git)
+```
+$ Open Terminal (Ctrl Alt T)
+$ git clone link 
+$ cd directory-name(Password)
+$ code .
+```
+
+
+## How to contribute
+You can fork the project from the git hub and clone it by following the above instruction
+
+
+## License and Copyright
+Licensed under[MIT License](LICENSE)Copyright &2020
+
+
---
+++ config.py
@@ -0,0 +1,60 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:annblue@localhost/pitch_test'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:annblue@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    #email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch'
+    SENDER_EMAIL = 'annaadhiambo01gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:annblue@localhost/pitch'
+
+    
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:annblue@localhost/pitches'
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:annblue@localhost/pitches'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test': TestConfig
+}
