052a76e7e5cb35f39d8b2cfddf85982692581c6e
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-25 13:04:35 +0300

    updated app folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_simplemde import SimpleMDE
-import os
 
 
-bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -17,6 +14,7 @@ login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
 mail = Mail()
 simple = SimpleMDE()
+
  
 
 def create_app(config_name):
--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Comment, Pitch, PitchCategory
+from app.models import User, Comment, Pizza, PizzaCategory
 
-
-
-app = create_app("production")
+app = create_app("development")
+# app = create_app("production")
 
 manager = Manager(app)
 manager.add_command('server',Server)
@@ -21,7 +20,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Pitch = Pitch, PitchCategory = PitchCategory)
+    return dict(app = app, db = db, User = User, Pizza = Pizza, PizzaCategory = PizzaCategory)
 
     
 if __name__ == '__main__':
